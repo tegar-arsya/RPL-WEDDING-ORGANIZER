5c359dbee4b93ba3def2090454125156b0180329
tegar-arsya <82997095+tegar-arsya@users.noreply.github.com>
2023-04-04 16:49:16 +0700

    Update helper.php

--- API_RPL/CRUD/helper.php
+++ API_RPL/CRUD/helper.php
@@ -1,1 +1,13 @@
+<?php
+
+
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('DB', 'rpl');
+
+    $db_connect = mysqli_connect( HOST, USER, PASS, DB) or die ('TIDAK konek');
+
+    header('Content-Type: application/json');
+?>
 
